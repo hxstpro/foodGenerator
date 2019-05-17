f4e711d5bf54b4144507b6b18711f3bdddbf238b
hxstpro <37120801+hxstpro@users.noreply.github.com>
2019-05-17 15:55:49 -0400

    Update foodGenerator.py

--- foodGenerator.py
+++ foodGenerator.py
@@ -5,17 +5,21 @@ import random
 foodPlaces = []
 with open('foodPlaces.txt') as filehandle:
         foodPlaces = [current_place.rstrip() for current_place in filehandle.readlines()]
+
 insult= []
 with open('insults.txt') as filehandle:
         insult = [current_place.rstrip() for current_place in filehandle.readlines()]
-        
+
 foodCost = round(random.uniform(10, 69))
 
 response = 'n'
+
 while response == 'n':
         ranPlace = random.choice(foodPlaces)
         ranInsult = random.choice(insult)
+        ranInsult2 = random.choice(insult)
         print('Do you wanna eat ' + str(ranPlace) + ', ' +ranInsult + "?")
         response = input().lower()
         print()
-print('Enjoy your $' + str(foodCost) + ' of ' + ranPlace + ', ' + ranInsult + ". See you tomorrow!")
+
+print('Enjoy your $' + str(foodCost) + ' of ' + ranPlace + ', ' + ranInsult2 + ". See you tomorrow!")
