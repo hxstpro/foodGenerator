5b62007dd5f693db7934dd1daeaa45fedfaba552
hxstpro <37120801+hxstpro@users.noreply.github.com>
2019-05-22 13:37:07 -0400

    Cleaned up code

--- foodGenerator.py
+++ foodGenerator.py
@@ -1,28 +1,20 @@
-#! python3
-
-import random
-import time
+import random, os, time
 
 foodPlaces = []
 with open('foodPlaces.txt') as filehandle:
-        foodPlaces = [current_place.rstrip() for current_place in filehandle.readlines()]
+    foodPlaces = [current_place.rstrip() for current_place in filehandle.readlines()]
 
 insult= []
 with open('insults.txt') as filehandle:
-        insult = [current_place.rstrip() for current_place in filehandle.readlines()]
+    insult = [current_place.rstrip() for current_place in filehandle.readlines()]
 
 foodCost = round(random.uniform(10, 69))
 
-response = 'n'
-
-while response == 'n':
-        ranPlace = random.choice(foodPlaces)
-        ranInsult = random.choice(insult)
-        ranInsult2 = random.choice(insult)
-        print('Do you wanna eat ' + str(ranPlace) + ', ' +ranInsult + "?")
-        response = input().lower()
-        print()       
-print('Enjoy your $' + str(foodCost) + ' of ' + ranPlace + ', ' + ranInsult2 + ". See you tomorrow!")
+response = ''
+while response != 'y':
+    print('Do you wanna eat ' + str(random.choice(foodPlaces)) + ', ' + random.choice(insult) + "?")
+    response = input().lower()
 print()
+print('Enjoy your $' + str(foodCost) + ' of ' + random.choice(foodPlaces) + ', ' + random.choice(insult) + ". See you tomorrow!")
 
 time.sleep(3)
